b222351be4a5c9a67ca13d3f09fd899a7220a329
Mary Faith Mitchell <maryfaithm19@tamu.edu>
2021-09-11 19:15:31 -0500

    radio button functionality added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flask import g
 
 app = Flask(__name__)
@@ -7,8 +7,8 @@ app = Flask(__name__)
 # reference for Flask.g: https://www.kite.com/python/docs/flask.g 
 # 2. https://www.notion.so/How-to-run-our-app-be3740b6406741888b89b8727acb3739 
 # 3. Python dictionaries: https://www.w3schools.com/python/python_dictionaries.asp
-
-
+# 4. Get/Post Tutorial https://www.askpython.com/python-modules/flask/flask-forms
+# 5. Radio button tutorial https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_radio
 
 @app.route("/")
 def first():
@@ -17,5 +17,23 @@ def first():
 
     return render_template("index.html", information=information)
 
-# @app.route("/second")
-# def second():
+@app.route("/second", methods = ["POST"])
+def second():
+    if request.method == "POST":
+        form_data = request.form
+        return render_template("second.html", form_data=form_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
